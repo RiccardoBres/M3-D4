7a7c7efd82976b5f74703436f96f5521c198167a
Riccardo Bresolin <124574800+RiccardoBres@users.noreply.github.com>
2023-05-01 20:18:11 +0100

    aggiunta funzionalità mostra/riduci- pagina SpecialOffers - sezzione: offerte del giorno

---
+++ M3-D4.js
@@ -0,0 +1,25 @@
+/*let button = document.querySelector("button");
+let offerte = document.getElementById("offerte")
+let div = document.createElement("div")
+let nuoveOfferte = ` <div class="row d-none d-md-flex" id="offerte" style="height: 400px">
+<div class="col-8 rounded-3 orizzCardImg">
+    <!-- <img src="assets/camel.jpg" class="img-fluid rounded-0" alt="..."> -->
+</div>
+<div class="col-4 orizzCard">
+    <h1>Marocco</h1>
+    <p>18 Giugno - 25 Giungo</p>
+    <h3>1.100 $</h3>
+    <p class="text-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis hic molestiae officia ea
+        libero veniam maiores ab non animi doloribus, doloremque harum molestias velit, eius, porro sint
+        minus tempore dolorum!
+    </p>
+    <button type="button" class="btn btn-primary">Next</button>
+        
+</div>
+</div>`
+
+button.addEventListener("click", function(){
+    offerte.remove();
+    div.innerHTML = nuoveOfferte;
+    console.log(div);
+})*/
