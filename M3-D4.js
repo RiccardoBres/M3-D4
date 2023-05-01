771d64f39b56abe3af668dea2487eb7b6b45a2f3
Riccardo Bresolin <124574800+RiccardoBres@users.noreply.github.com>
2023-05-01 20:57:34 +0100

    aggiunta modale pagina testimonial- sezione Contact

--- M3-D4.js
+++ M3-D4.js
@@ -1,14 +1,13 @@
+/*///////// Funzione conteggio Offerte del giorno ////////////*/
+
 let card = document.getElementsByClassName("card")
 let title = document.getElementById("title")
 
-/*for(i=0; i<card.length; i++) {
-   let newTitle = "Le offerte della settimana : " + card.lenght;
-   console.log(newTitle);
-}
-*/
 if (card.length > 0) {
     const newTitle = `Le offerte della settimana sono: ${card.length}`;/*${card.length} viene utilizzato per inserire all'interno della stringa Le offerte della settimana: ".."" il valore della proprietà length*/
     title.innerText = newTitle;
   } else {
     console.log('Nessuna offerta trovata.');
   }
+
+/*/////////////////////////////////////////////////////////////*/
